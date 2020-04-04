77be46fdcc5dc8ae4bd22da2147b0c9f92a0a578
Theophane Durand <theophane.durand@etu.utc.fr>
2020-04-04 19:36:38 +0200

    Update suppressionTables.sql

--- suppressionTables.sql
+++ suppressionTables.sql
@@ -1,9 +1,19 @@
+DROP TRIGGER IF EXISTS trig_insff_OS ON OS cascade;
+DROP TRIGGER IF EXISTS trig_insff_Programme ON Programme cascade;
 DROP TABLE IF EXISTS Logiciel cascade;
 DROP TABLE IF EXISTS Version cascade;
 DROP TABLE IF EXISTS Probleme cascade;
-
 DROP TABLE IF EXISTS Client cascade;
 DROP TABLE IF EXISTS Achat cascade;
 DROP TABLE IF EXISTS Correctif cascade;
 DROP TABLE IF EXISTS Employe cascade;
 DROP TABLE IF EXISTS RemonterProbleme cascade;
+DROP TABLE IF EXISTS OS cascade;
+DROP TABLE IF EXISTS Programme cascade;
+DROP TABLE IF EXISTS SupporterOProg cascade;
+DROP TABLE IF EXISTS ProgramType cascade;
+DROP TABLE IF EXISTS Plateforme cascade;
+DROP TABLE IF EXISTS SupporterOPlat cascade;
+DROP TABLE IF EXISTS EstDeType cascade;
+DROP TYPE IF EXISTS TYPE_t cascade;
+DROP FUNCTION IF EXISTS fonction_trigger;
